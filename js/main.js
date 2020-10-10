fbeb9461a58c05048a14373dcac77c0290cc371a
julettas <juletta179@gmail.com>
2020-10-10 09:09:43 +0300

    Личный проект: больше деталей (часть 2)

--- js/main.js
+++ js/main.js
@@ -84,3 +84,53 @@ let MapPins = getMapPins();
 map.querySelector(`.map__pins`).appendChild(createMapPinFragment());
 map.classList.remove(`map--faded`);
 
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // если тело цикла начнет выполняться - значит в объекте есть свойства
+    return false;
+  }
+  return true;
+}
+
+const renderCardElement = function (card) {
+  let cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+  let cardElement = cardTemplate.cloneNode(true);
+  card.title ? cardElement.querySelector(`.popup__title`).textContent = card.title : cardElement.querySelector(`.popup__title`).remove();
+  card.offer.address ? cardElement.querySelector(`.popup__text--address`).textContent = card.offer.address : cardElement.querySelector(`.popup__text--address`).remove();
+  card.offer.price ? cardElement.querySelector(`.popup__text--price`).textContent = card.offer.price + `₽/ночь` : cardElement.querySelector(`.popup__text--price`).remove();
+  (card.offer.rooms && card.offer.guests) ? cardElement.querySelector(`.popup__text--capacity`).textContent = card.offer.rooms + `комнаты для` + card.offer.guests + `гостей` : cardElement.querySelector(`.popup__text--capacity`).remove();
+  (card.offer.checkin && card.offer.checkout) ? cardElement.querySelector(`.popup__text--time`).textContent = card.offer.checkin + `, выезд до` + card.offer.checkout : cardElement.querySelector(`.popup__text--time`).remove();
+
+  let features = cardElement.querySelector(`.popup__features`);
+  let featureTemplate = features.querySelector(`.popup__feature`);
+  featureTemplate.classList.remove(`popup__feature--wifi`);
+  features.querySelectorAll(`.popup__feature`).forEach((el) => el.remove());
+  for (let i = 0; i < card.offer.features.length; i++) {
+    let feature = featureTemplate.cloneNode(true);
+    feature.classList.add(`popup__feature--` + card.offer.features[i]);
+    features.appendChild(feature);
+  }
+  card.offer.description ? cardElement.querySelector(`.popup__description`).textContent = card.offer.description : cardElement.querySelector(`.popup__description`).remove();
+  let photos = cardElement.querySelector(`.popup__photos`);
+  if (!isEmpty(card.offer.photos)) {
+    let photoTemplate = cardElement.querySelector(`.popup__photo`);
+    photos.querySelector(`.popup__photo`).remove();
+    for (let i = 0; i < card.offer.photos.length; i++) {
+      let photo = photoTemplate.cloneNode(true);
+      photo.src = card.offer.photos[i];
+      photos.appendChild(photo);
+    }
+  } else {
+    photos.remove();
+  }
+
+  card.author.avatar ? cardElement.querySelector(`.popup__avatar`).src = card.author.avatar: cardElement.querySelector(`.popup__avatar`).remove() ;
+
+  return cardElement;
+};
+console.log(MapPins[0]);
+console.log(MapPins[0].offer.features.length);
+document.querySelector(`.map__filters-container`).before(renderCardElement(MapPins[0]));
+
+
